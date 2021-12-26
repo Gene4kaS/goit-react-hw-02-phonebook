f3e0c085c2799352d171a9fa0619fd6dfaee45f4
Gene4kaS <jannasavchenko@gmail.com>
2021-12-26 23:50:49 +0200

    push wht styles

--- src/App.js
+++ src/App.js
@@ -1,58 +1,69 @@
 import './App.css';
 import React, { Component } from 'react';
-import Phonebook from './components/Phonebook';
+import ContactForm from './components/ContactForm';
 import shortid from 'shortid';
-import ContactsList from './components/ContactsList/ContactsList';
+import ContactsList from './components/ContactsList';
+import Filter from './components/Filter';
 
 class App extends Component {
   state = {
-    contacts: [],
-    name: '',
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
   };
 
-  addContact = text => {
+  addContact = data => {
+    const { contacts } = this.state;
+    const { name, number } = data;
     const contact = {
       id: shortid.generate(),
-      text,
-      completed: false,
+      name,
+      number,
     };
+
+    if (contacts.some(({ name }) => name === contact.name)) {
+      alert(`Sorry, ${name} already exists`);
+      return;
+    }
+
     this.setState(({ contacts }) => ({
       contacts: [contact, ...contacts],
     }));
   };
 
-  toggleCompleted = contactId => {
-    // this.setState(prevState => ({
-    //   todos: prevState.todos.map(todo => {
-    //     if (todo.id === todoId) {
-    //       return {
-    //         ...todo,
-    //         completed: !todo.completed,
-    //       };
-    //     }
+  deleteContact = contactId => {
+    const state = this.state;
+    const visibleContacts = state.contacts.filter(
+      contact => contact.id !== contactId,
+    );
+    this.setState({ contacts: visibleContacts });
+    return visibleContacts;
+  };
 
-    //     return todo;
-    //   }),
-    // }));
-
-    this.setState(({ contacts }) => ({
-      contacts: contacts.map(contact =>
-        contact.id === contactId
-          ? { ...contact, completed: !contact.completed }
-          : contact,
-      ),
-    }));
+  onFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
   };
 
   render() {
+    const { contacts, filter } = this.state;
+    const visibleContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase()),
+    );
     return (
       <>
-        <div>
+        <div className="phonebook">
           <h1>Phonebook</h1>
-          <Phonebook onAddContact={this.addContact} />
-
+          <ContactForm onAddContact={this.addContact} />
+          <Filter value={filter} onChange={this.onFilter} />
           <h2>Contacts</h2>
-          <ContactsList onToggleCompleted={this.toggleCompleted} />
+          <ContactsList
+            contacts={visibleContacts}
+            deleteContact={this.deleteContact}
+          />
         </div>
       </>
     );
--- src/components/ContactsList/ContactsList.js
+++ src/components/ContactsList/ContactsList.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-const ContactsList = ({ id, text }) => (
-  <ul>
-    <li key={id}>{text}</li>
-  </ul>
-);
-
+const ContactsList = ({ contacts, deleteContact }) => {
+  return (
+    <ul>
+      {contacts.map(contact => {
+        return (
+          <li key={contact.id}>
+            {contact.name} {contact.number}
+            <button type="button" onClick={() => deleteContact(contact.id)}>
+              Delete
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 export default ContactsList;
